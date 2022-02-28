1d92fe297c40066ca66a543d8df5662a32d3bf6b
Gabriel <57547790+programacaogabriel@users.noreply.github.com>
2022-02-28 08:40:44 -0300

    create domain class

---
+++ src/main/java/br/com/gabrielmartinsdasilva/MyAPI/domain/Usuario.java
@@ -0,0 +1,97 @@
+package br.com.gabrielmartinsdasilva.MyAPI.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull(message = "Campo NOME é mandatório")
+	@Length(min = 3, max=100, message = "O nome deve ter entre 3 e 100 caracteres")
+	private String nome;
+	
+	@NotNull(message = "Campo LOGIN é mandatório")
+	@Length(min = 3, max=100, message = "O nome deve ter entre 3 e 100 caracteres")
+	private String login;
+	
+	@NotNull(message = "Campo SENHA é mandatório")
+	@Length(min = 3, max=100, message = "O nome deve ter entre 3 e 100 caracteres")
+	private String senha;
+
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(Integer id, String nome, String login, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ src/test/java/br/com/gabrielmartinsdasilva/MyAPI/MyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmartinsdasilva.MyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
